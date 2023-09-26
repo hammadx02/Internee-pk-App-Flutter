4606e4f36416439657156249e8a7866edcd4f41d
hammadx02 <hammadaali02@gmail.com>
2023-09-27 00:39:36 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
     );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
