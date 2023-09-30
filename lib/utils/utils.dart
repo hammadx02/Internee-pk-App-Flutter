60b73f1e0117dda5188600c1ef3ec386b99f3f8a
hammadx02 <hammadaali02@gmail.com>
2023-09-30 14:46:52 +0500

    updated

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
-
-
-
 class Utils {
   void toastMessage(String message) {
     Fluttertoast.showToast(
